af2f8f55e108c7a5655de81192f105872d42b042
raphpay <raphpay.dev@gmail.com>
2024-01-24 09:05:38 +0100

    first commit

---
+++ Kodeco-Book-iOS/Kodeco_Book_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kodeco_Book_iOSApp.swift
+//  Kodeco-Book-iOS
+//
+//  Created by Raphaël Payet on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Kodeco_Book_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
